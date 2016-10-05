63a960e8d96eac5d5222a4ea6aafc9d303b6fdd4
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-05 15:53:53 -0300

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='ovp-users',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=['ovp_users'],
     url='https://github.com/OpenVolunteeringPlatform/ovp-users',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/ovp-users/tarball/0.1.2',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/ovp-users/tarball/0.1.3',
     license='AGPL',
     description='This module has core functionality for' + \
                 ' ovp users, such as authentication, user management' + \
