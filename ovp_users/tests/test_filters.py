857809c058764eed408ce4d0e15c4eac8cb06848
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-06 15:27:00 -0300

    Update filters tests

--- ovp_users/tests/test_filters.py
+++ ovp_users/tests/test_filters.py
@@ -3,10 +3,15 @@ from django.test import TestCase
 from ovp_users.recover_password import RecoveryTokenFilter
 from ovp_users.recover_password import RecoverPasswordFilter
 
+def test_filter(c):
+  obj = c()
+  obj.filter_queryset('a', 'b', 'c')
+  obj.get_fields('a')
+
 def TestPasswordRecoveryFilters(TestCase):
   def test_filters():
     """Assert filters do not throw error when instantiated"""
     # Nothing to assert here, we just instantiate them and
     # make sure it throws no error
-    RecoveryTokenFilter()
-    RecoverPasswordFilter()
+    test_filter(RecoveryTokenFilter)
+    test_filter(RecoverPasswordFilter)
