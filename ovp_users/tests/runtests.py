2b3058348c565d097bda1395408dc63671786d79
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-07 16:42:26 -0300

    Update runtests.py to prioritize local module instead of pip module

--- ovp_users/tests/runtests.py
+++ ovp_users/tests/runtests.py
@@ -9,7 +9,7 @@ from django.core.management import execute_from_command_line
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.abspath(os.path.join(BASE_DIR, '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(BASE_DIR, '../..')))
 
 # Unfortunately, apps can not be installed via ``modify_settings``
 # decorator, because it would miss the database setup.
