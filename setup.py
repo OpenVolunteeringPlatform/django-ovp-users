27c150a9b70025aa284389902a3e898bee79a7bc
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-07 16:59:10 -0200

    Add ovp-uploads as dependency

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
       'python-dateutil>=2.5.3,<2.6.0',
       'codecov>=2.0.5,<2.1.0',
       'coverage>=4.2,<4.3.0',
-      'ovp-core>=1.0.1,<2.0.0'
+      'ovp-core>=1.0.1,<2.0.0',
+      'ovp-uploads>=1.0.0,<2.0.0'
     ]
 )
