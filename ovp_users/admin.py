a57c3eaf7f2651de833829e151cfdf968ef9524f
Cidiomar Jr <cidiomar.jr@gmail.com>
2017-01-09 18:36:07 -0200

    Implement admin panel

--- ovp_users/admin.py
+++ ovp_users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from ovp_users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+  fields = [
+    ('id', 'name', 'email'), 'slug', 'phone',
+    ('is_staff','is_superuser','is_active','is_email_verified',)
+    ]
+
+  list_display = [
+    'id', 'email', 'name', 'last_login', 'is_active', 'is_staff', 'is_email_verified'
+    ]
+
+  list_filter = [
+    'is_active', 'is_staff', 'last_login', 'joined_date'
+    ]
+
+  list_editable = [
+    'is_active', 'is_staff', 'is_email_verified'
+    ]
+
+  search_fields = [
+    'email', 'name'
+    ]
+
+  readonly_fields = ['id']
+  raw_id_fields = []
+
+
+admin.site.register(User, UserAdmin)
+
+
