8a57a79f1ae2f9eefb93da392927ccbf791ed057
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-02-21 11:03:13 -0300

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='ovp-users',
-    version='1.1.0',
+    version='1.1.1',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/OpenVolunteeringPlatform/ovp-users',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/ovp-users/tarball/1.1.0',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/ovp-users/tarball/1.1.1',
     license='AGPL',
     description='This module has core functionality for' + \
                 ' ovp users, such as authentication, user management' + \
