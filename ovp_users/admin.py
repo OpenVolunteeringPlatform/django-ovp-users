d719a570f5f04f5927de419d6b0c0ce6c6ece622
Fabio Garcia <fabioo.salgado@gmail.com>
2018-05-03 11:05:17 -0300

    Add phone2 admin

--- ovp_users/admin.py
+++ ovp_users/admin.py
@@ -7,8 +7,8 @@ from ovp_users.models import User
 
 class UserAdmin(admin.ModelAdmin):
   fields = [
-    ('id', 'name', 'email'), 'slug', 'phone',
-    ('is_staff','is_superuser','is_active','is_email_verified','public',),
+    ('id', 'name', 'email'), 'slug', 'phone', 'phone2',
+    ('is_staff','is_superuser','is_active','is_email_verified','public'),
     ('password')
     ]
 
